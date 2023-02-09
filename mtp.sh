555ae5a841ba819e97f10d717b2fb597c3f6a978
MatrixLau <matrixlj0113@gmail.com>
2023-02-09 22:11:07 +0800

    Update mtp.sh

--- mtp.sh
+++ mtp.sh
@@ -1,7 +1,12 @@
 #!/bin/bash                                                                                                  
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
-read -e -p "请输入链接端口(默认443) :" port
+read -e -p "请输入MTP白名单添加网页端口(默认80) :" nport
+if [[ -z "${nport}" ]]; then
+nport="80"
+fi
+
+read -e -p "请输入MTP链接端口(默认443) :" port
 if [[ -z "${port}" ]]; then
 port="443"
 fi
@@ -17,6 +22,21 @@ read -e -p "请输入伪装域名(默认www.microsoft.com) :" domain
 if [[ -z "${domain}" ]]; then
 domain="www.microsoft.com"
 fi
+
+read -e -p "请输入可访问IP(1.单IP白名单 2.IP段白名单 3.不设置[默认]):" iplist
+case $iplist in
+    1) 
+    iplist="IP"
+    ;;
+    2)
+    iplist="IPSEG"
+    ;;
+    *)
+    iplist="OFF"
+    ;;
+esac
+echo "选择了$iplist模式~"
+
 read -rp "你需要TAG标签吗(Y/N): " chrony_install
     [[ -z ${chrony_install} ]] && chrony_install="Y"
     case $chrony_install in
@@ -28,7 +48,7 @@ read -rp "你需要TAG标签吗(Y/N): " chrony_install
         echo -e "正在安装依赖: Docker... "
         echo y | bash <(curl -L -s https://raw.githubusercontent.com/xb0or/nginx-mtproxy/main/docker.sh)
         echo -e "正在安装nginx-mtproxy... "
-        docker run --name nginx-mtproxy -d -e tag="$tag" -e secret="$secret" -e domain="$domain" -p 80:80 -p $port:$port ellermister/nginx-mtproxy:latest
+        docker run --name nginx-mtproxy -d -e tag="$tag" -e secret="$secret" -e domain="$domain" -e ip_white_list="$iplist" -p $nport:80 -p $port:8443 ellermister/nginx-mtproxy:latest
         ;;
     *)
     #-v /etc/nginx:/etc/nginx 
@@ -36,7 +56,7 @@ echo -e "正在安装依赖: Docker... "
 echo y | bash <(curl -L -s https://cdn.jsdelivr.net/gh/xb0or/nginx-mtproxy@main/docker.sh)
 
 echo -e "正在安装nginx-mtproxy... "
-docker run --name nginx-mtproxy -d -e secret="$secret" -e domain="$domain" -p 80:80 -p $port:$port ellermister/nginx-mtproxy:latest
+docker run --name nginx-mtproxy -d -e secret="$secret" -e domain="$domain" -e ip_white_list="$iplist" -p $nport:80 -p $port:8443 ellermister/nginx-mtproxy:latest
         ;;
     esac
 
@@ -53,5 +73,5 @@ docker update --restart=always nginx-mtproxy
     echo -e "服务器IP：\033[31m$public_ip\033[0m"
     echo -e "服务器端口：\033[31m$port\033[0m"
     echo -e "MTProxy Secret:  \033[31m$client_secret\033[0m"
-    echo -e "TG认证地址：http://${public_ip}:80/add.php"
+    echo -e "TG认证地址：http://${public_ip}:${nport}/add.php"
     echo -e "TG一键链接: https://t.me/proxy?server=${public_ip}&port=${port}&secret=${client_secret}"
